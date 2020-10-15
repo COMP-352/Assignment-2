a51343721b1222e725822cfeff98e912b35a286a
antistarM <61073494+antistarM@users.noreply.github.com>
2020-10-15 01:01:59 -0400

    Update magicBoard.java

--- magicBoard.java
+++ magicBoard.java
@@ -1,10 +1,11 @@
 package A2;
 
 import java.util.Random;
-//  ** Note this is just to fill the board with a random length between 5-20 and random numbers n->n-1. Still have to figure out
-//  ** how we can traverse the array 
+
 public class magicBoard {
 
+//  ** Note this is just to fill the board with a random length between 5-20 and random numbers n->n-1. Still have to figure out
+//  ** how we can traverse the array
     public static void createBoard()
     {
         Random random = new Random();
@@ -12,26 +13,26 @@ public class magicBoard {
         int max = 20;
 
         //  To randomize array row length:
-        final int row = random.nextInt(max + 1 - min) +min ;
+        final int row = random.nextInt(max + 1 - min) + min ;
         //  Collum length dependent on row length
-        final int col = row -1 ;
+        final int col = row - 1 ;
 
         int [][] board = new int [row][col];
 
         //  Two for loops to fill the board
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = (int)(Math.random()*19);
+                board[i][j] = (int)(Math.random() * board.length - 1);
             }
         }
 
         //  Two for loops to display the board output
-        for(int i=0;i<board.length-1; i++)
+        for(int i = 0;i < board.length - 1; i++)
         {
-            for(int j=0; j<board[i].length; j++)
-            System.out.print(board[i][j]+"\t ");
-            System.out.println();
-            System.out.println();
+            for(int j = 0; j < board[i].length; j++)
+                System.out.print(board[i][j]+"\t ");
+                System.out.println();
+                System.out.println();
         }
 
     }
